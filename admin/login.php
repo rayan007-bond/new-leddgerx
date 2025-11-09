4c48dab0dfb740f2dc3bfa969ade8e6b4a69c27d
Muhammad Rayan <129910583+rayan007-bond@users.noreply.github.com>
2025-11-09 11:13:11 +0500

    Add files via upload

--- admin/login.php
+++ admin/login.php
@@ -2,15 +2,18 @@
 session_start();
 header('Content-Type: application/json');
 
-// Database configuration
-define('DB_HOST', 'sql305.infinityfree.com');
-define('DB_USER', 'if0_40349999');
-define('DB_PASS', 'Fasihajan28');   // Enter your CP password here
-define('DB_NAME', 'if0_40349999_ledgerx_blog');
+$host = "mysql.railway.internal";
+$user = "root";
+$pass = "VqffNprPLnLvKpRCwPDqIdkQeDIEkRUe";
+$db   = "VqffNprPLnLvKpRCwPDqIdkQeDIEkRUe";
+$port = "3306";
 
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Create connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 // Check connection
 if ($conn->connect_error) {
